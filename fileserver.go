ce3d74e402039710a75b7aed98cbba61b7f27b58
StevenZack <stevenzack@qq.com>
2018-11-17 04:07:07 +0800

    Just a Backup

--- fileserver.go
+++ fileserver.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/StevenZack/tools/netToolkit"
 	"net/http"
 	"os"
 )
@@ -16,6 +17,9 @@ func main() {
 		port = os.Args[2]
 	}
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(dir))))
+	for _, ip := range netToolkit.GetIPs() {
+		fmt.Println("listened on ", ip+port)
+	}
 	e := http.ListenAndServe(port, nil)
 	if e != nil {
 		fmt.Println("listen error:", e)
