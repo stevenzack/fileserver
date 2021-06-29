6927759e3e224f0419bec3f3746c4a80158f5ec7
stevenzack <zigzigcheers@gmail.com>
2021-06-29 13:28:36 +0800

    index-support

--- fileserver.go
+++ fileserver.go
@@ -5,19 +5,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 	"text/template"
 
+	"github.com/StevenZack/mux"
 	"github.com/StevenZack/openurl"
 	"github.com/StevenZack/tools/fileToolkit"
 	"github.com/StevenZack/tools/netToolkit"
 )
 
 var (
-	port        = flag.String("p", ":8080", "port")
+	port        = flag.String("p", "8080", "port")
 	dir         = flag.String("d", ".", "dir to serve")
 	useTemplate = flag.Bool("t", false, "Use template")
+	index       = flag.Bool("i", false, "Route 404 to index.html")
+	s           *mux.Server
 )
 
 type fileHandler struct {
@@ -31,22 +35,50 @@ func init() {
 func main() {
 	flag.Parse()
 
+	s = mux.NewServer(":" + *port)
 	if *useTemplate {
-		http.Handle("/", &fileHandler{root: *dir})
+		s.Handle("/", &fileHandler{root: *dir})
 		fmt.Println("using template engine")
 	} else {
-		http.Handle("/", http.FileServer(http.Dir(*dir)))
+		s.HandleMultiReqs("/", fs)
 	}
 
 	for _, ip := range netToolkit.GetIPs(false) {
-		fmt.Println("listened on ", ip+*port)
+		fmt.Println("listened on ", ip+":"+*port)
 	}
-	openurl.Open("http://localhost" + *port)
-	e := http.ListenAndServe(*port, nil)
+	openurl.Open("http://localhost:" + *port)
+	e := s.ListenAndServe()
 	if e != nil {
 		fmt.Println("listen error:", e)
 		return
 	}
+}
+
+func fs(w http.ResponseWriter, r *http.Request) {
+	p := r.URL.Path
+	if p == "/" {
+		p = "/index.html"
+	}
+	path := filepath.Join(*dir, p)
+	info, e := os.Stat(path)
+	if e != nil {
+		if os.IsNotExist(e) {
+			if *index {
+				http.ServeFile(w, r, filepath.Join(*dir, "index.html"))
+				return
+			}
+			s.NotFound(w, r)
+			return
+		}
+		log.Println(e)
+		http.Error(w, e.Error(), http.StatusInternalServerError)
+		return
+	}
+	if info.IsDir() {
+		http.Error(w, "dir unsupported", http.StatusInternalServerError)
+		return
+	}
+	http.ServeFile(w, r, path)
 }
 
 func (f *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/StevenZack/fileserver
 go 1.15
 
 require (
+	github.com/StevenZack/mux v1.2.1
 	github.com/StevenZack/openurl v0.0.0-20190430065139-b25363f65ff8
 	github.com/StevenZack/tools v1.9.21
 )
